9cd6f7d3067f03dc77902c24d377dd0bbe8a6271
CS-LIKHITH <95606643+CS-LIKHITH@users.noreply.github.com>
2025-10-22 12:56:30 +0530

    Update 311_JS.js

--- 311_JS.js
+++ 311_JS.js
@@ -1,32 +1,10 @@
-import fs from "fs";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-const userData = {
-  username: "johndoe",
-  password: "SuperSecretPassword123"
-};
-const s3 = new S3Client({
-  region: "us-east-1",
-  endpoint: "http://insecure-local-s3.example",
-  forcePathStyle: true,
+secmgr.createSecret(params, (err, data) => {
+    if (err) console.log(err);
+    else {
+        console.log(data);
+        res.end(data.ARN);
+        res.end(data.Name);
+        res.end(data.VersionId);
+        res.end(secretValue);
+    }
 });
-
-async function uploadUserData(data) {
-  const key = `userdata/${data.username}.json`;
-  const body = JSON.stringify(data);
-
-  const cmd = new PutObjectCommand({
-    Bucket: "my-demo-bucket",
-    Key: key,
-    Body: body,
-    ContentType: "application/json"
-  });
-
-  try {
-    const resp = await s3.send(cmd);
-    console.log("Upload response:", resp);
-  } catch (err) {
-    console.error("Upload failed:", err);
-  }
-}
-
-uploadUserData(userData);
